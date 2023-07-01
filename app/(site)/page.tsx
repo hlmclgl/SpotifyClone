35160e526eebba5a08d602be3216f3b42b8f90eb
hlmclgl <hlmclgl@gmail.com>
2023-07-02 02:20:44 +0300

    01.07.2023 last commit

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,13 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import PageContent from "./components/PageContent";
 
+export const revalidate = 0;
 
-export default function Home() {
+export default async function Home() {
+  const songs = await getSongs();
+  
   return (
     <div className="
       bg-neutral-900
@@ -49,7 +54,7 @@ export default function Home() {
           </h1>
         </div>
         <div>
-          List of Songs !!!
+          <PageContent songs={songs} />
         </div>
       </div>
     </div>
