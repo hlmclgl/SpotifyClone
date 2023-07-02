edeb4fa8b96ff69a20791b854d2df403861db12b
hlmclgl <hlmclgl@gmail.com>
2023-07-02 22:48:29 +0300

    Added Songs play

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,6 +7,7 @@ import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 
 interface LibraryProps {
@@ -20,6 +21,7 @@ const Library: React.FC<LibraryProps> = ({
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const { user } = useUser();
+    const onPlay = useOnPlay(songs);
 
     const onClick = () =>{
         if (!user) {
@@ -79,7 +81,7 @@ const Library: React.FC<LibraryProps> = ({
             ">
                 {songs.map((item) => (
                     <MediaItem 
-                        onClick={() => {}} 
+                        onClick={(id: string) => onPlay(id)} 
                         key={item.id} 
                         data={item}
                     />
